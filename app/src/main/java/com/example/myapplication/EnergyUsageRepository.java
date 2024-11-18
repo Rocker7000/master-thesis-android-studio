ef15044e273a77732476359e280811c87cc0b485
Borys <Shaman4ik7000@gmail.com>
2024-11-18 15:11:43 +0200

    Added functionality for Analysis page.

--- app/src/main/java/com/example/myapplication/EnergyUsageRepository.java
+++ app/src/main/java/com/example/myapplication/EnergyUsageRepository.java
@@ -12,9 +12,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EnergyUsageRepository {
@@ -24,6 +27,7 @@ public class EnergyUsageRepository {
     private final Map<String, Float> dailyTotalsMap = new LinkedHashMap<>();
     private final Map<String, Map<String, Float>> dailyDeviceUsageMap = new HashMap<>();
     private final Map<String, Map<String, Float>> hourlyDevicesUsageMap = new HashMap<>();
+    private final Map<String, Integer> deviceUsageCountMap = new HashMap<>();
     private String userNickname, userEmail, userPhoneNumber, userPhotoUrl;
     private boolean isDataLoaded = false;
     private FirebaseUser user;
@@ -52,6 +56,7 @@ public class EnergyUsageRepository {
     public String getUserPhoneNumber() {
         return userPhoneNumber;
     }
+
     public String getUserPhotoUrl() {
         return userPhotoUrl;
     }
@@ -122,6 +127,101 @@ public class EnergyUsageRepository {
         return isDataLoaded;
     }
 
+    public List<Map<String, Object>> getDeviceStatistics() {
+        List<Map<String, Object>> deviceStatisticsList = new ArrayList<>();
+        Map<String, Map<String, Float>> dailyDeviceUsageMap = getDailyDeviceUsageMap();
+
+        for (Map.Entry<String, Map<String, Float>> entry : dailyDeviceUsageMap.entrySet()) {
+            for (Map.Entry<String, Float> deviceEntry : entry.getValue().entrySet()) {
+                String deviceName = deviceEntry.getKey();
+                Float usage = deviceEntry.getValue();
+                if (usage > 0) {
+                    deviceStatisticsList.add(Map.of("device", deviceName, "usage", usage));
+                }
+            }
+        }
+
+        Map<String, List<Float>> deviceUsagesMap = new HashMap<>();
+        for (Map<String, Object> entry : deviceStatisticsList) {
+            String device = (String) entry.get("device");
+            Float usage = (Float) entry.get("usage");
+            deviceUsagesMap.computeIfAbsent(device, k -> new ArrayList<>()).add(usage);
+        }
+
+        List<Map<String, Object>> result = new ArrayList<>();
+        for (Map.Entry<String, List<Float>> entry : deviceUsagesMap.entrySet()) {
+            String device = entry.getKey();
+            List<Float> usages = entry.getValue();
+            Collections.sort(usages);
+
+            float average = averageConsumption(usages);
+            float standardDeviation = standardDeviation(usages, average);
+            float min = min(usages);
+            float percentile25 = percentile(usages, 25);
+            float median = percentile(usages, 50);
+            float percentile75 = percentile(usages, 75);
+            float max = max(usages);
+
+            result.add(Map.of(
+                    "device", device,
+                    "count", getDeviceUsageInTimeFormat(device),
+                    "average", average,
+                    "standardDeviation", standardDeviation,
+                    "min", min,
+                    "percentile25", percentile25,
+                    "median", median,
+                    "percentile75", percentile75,
+                    "max", max
+            ));
+        }
+
+        return result;
+    }
+
+
+    private String getDeviceUsageInTimeFormat(String device) {
+        // Example: return in "hour:minute" format
+        // if count if 1 then it means that device was used for 10 minutes
+
+        int averageCount = (deviceUsageCountMap.getOrDefault(device, 0) / dailyTotalsMap.size());
+        int hours = averageCount / 6;
+        int minutes = (averageCount % 6) * 10;
+
+        return hours + ":" + (minutes >= 10 ? minutes : minutes + "0");
+
+
+    }
+
+    private float averageConsumption(List<Float> usages) {
+        float sum = 0;
+        for (Float usage : usages) {
+            sum += usage;
+        }
+        return sum / usages.size();
+    }
+
+    private float standardDeviation(List<Float> usages, float average) {
+        float sumSquaredDifferences = 0;
+        for (Float usage : usages) {
+            sumSquaredDifferences += Math.pow(usage - average, 2);
+        }
+        return (float) Math.sqrt(sumSquaredDifferences / usages.size());
+    }
+
+    private float min(List<Float> usages) {
+        return Collections.min(usages);
+    }
+
+    private float percentile(List<Float> usages, float percentile) {
+        int index = (int) Math.ceil(percentile / 100.0 * usages.size()) - 1;
+        return usages.get(index);
+    }
+
+    private float max(List<Float> usages) {
+        return Collections.max(usages);
+    }
+
+
     public void loadLastMonth(Runnable onComplete) {
         if (user == null) {
             Log.e("EnergyUsageRepository", "Користувач не авторизований");
@@ -142,7 +242,7 @@ public class EnergyUsageRepository {
         dailyDeviceUsageMap.clear();
         hourlyDevicesUsageMap.clear();
 
-        Query last30DaysQuery = databaseRef.child("users").child(user.getUid()).child("houseEnergyUsage").orderByKey().limitToLast(30);
+        Query last30DaysQuery = databaseRef.child("users").child(user.getUid()).child("houseEnergyUsage").orderByKey();
 
         databaseRef.child("users").child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -170,7 +270,8 @@ public class EnergyUsageRepository {
                 for (DataSnapshot dateSnapshot : snapshot.getChildren()) {
                     String date = dateSnapshot.getKey();
                     float dailyTotal = 0.0f;
-                    Map<String, Float> deviceUsage = new HashMap<>();
+
+                    Map<String, Float> deviceTotalUsage = new HashMap<>();
                     Map<String, Float> hourlyUsage = new LinkedHashMap<>();
 
                     for (DataSnapshot timeSnapshot : dateSnapshot.getChildren()) {
@@ -181,21 +282,23 @@ public class EnergyUsageRepository {
                         float hourlyTotal = 0.0f;
 
                         for (DataSnapshot deviceSnapshot : timeSnapshot.getChildren()) {
-                            String deviceName = deviceSnapshot.getKey();
+                            String deviceName  = deviceSnapshot.getKey();
                             Double usage = deviceSnapshot.getValue(Double.class);
 
                             if (usage != null) {
                                 dailyTotal += usage;
                                 hourlyTotal += usage.floatValue();
-                                deviceUsage.put(deviceName, deviceUsage.getOrDefault(deviceName, 0f) + usage.floatValue());
+                                if (usage != 0) {
+                                    deviceUsageCountMap.put(deviceName, deviceUsageCountMap.getOrDefault(deviceName, 0) + 1);
+                                }
+                                deviceTotalUsage.put(deviceName, deviceTotalUsage.getOrDefault(deviceName, 0f) + usage.floatValue());
                             }
                         }
-
                         hourlyUsage.put(hourKey, hourlyUsage.getOrDefault(hourKey, 0f) + hourlyTotal);
                     }
 
                     dailyTotalsMap.put(date, dailyTotal);
-                    dailyDeviceUsageMap.put(date, deviceUsage);
+                    dailyDeviceUsageMap.put(date, deviceTotalUsage);
                     hourlyDevicesUsageMap.put(date, hourlyUsage);
                 }
 
